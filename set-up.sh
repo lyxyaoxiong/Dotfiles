9111d800f24877468de8698ea80a6415797af43b
Yaoxiong Liang <yx.liangim@gmail.com>
2020-12-12 12:15:17 -0800

    Initial commit

---
+++ set-up.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+# sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# .vimrc (https://github.com/amix/vimrc)
